71759013ff881410c22c00f5f879e854fae82725
staticnation <staticnation@gmail.com>
2023-05-22 01:31:48 -0700

    Update main.lua

--- MWSE/mods/Static/Set Bonus/main.lua
+++ MWSE/mods/Static/Set Bonus/main.lua
@@ -1,23 +1,28 @@
-local lfs = require('lfs')
-local config = require("Static.Set Bonus.config")
-local sets = config.sets
-local setLinks = config.setLinks
-local interop = require("Static.Set Bonus.init")
-local util = require("Static.Set Bonus.util")
+-- Importing modules
+local lfs = require('lfs') -- filesystem functions
+local config = require("Static.Set Bonus.config") -- your custom configuration
+local sets = config.sets -- sets from the configuration
+local setLinks = config.setLinks -- setLinks from the configuration
+local interop = require("Static.Set Bonus.init") -- your custom initialization code
+local util = require("Static.Set Bonus.util") -- your custom utility functions
 
+-- Function to load and register set data
 local function initAll(path)
-    path = "Data Files/MWSE/mods/Static/Set Bonus/" .. path .. "/"
+    path = "sets" .. path .. "/"
     for file in lfs.dir(path) do
         if file:match("(.+)%.lua$") then
-            local set = require("Static.Set Bonus." .. path .. file:match("(.+)%.lua$"))
+            local modulePath = path .. file:match("(.+)%.lua$")
+            mwse.log("Loading set file: %s", modulePath) -- Debug log
+            local set = require(modulePath)
             interop.registerSet(set)
         end
     end
 end
 
 -- Load set files using interop
-initAll("sets")
+initAll("sets") -- Make sure this "sets" folder is present in the project directory
 
+-- Loop over sets to create links
 for id, set in pairs(sets) do
     mwse.log("Creating links for set: %s", set.name)
     for _, item in ipairs(set.items) do
@@ -26,9 +31,11 @@ for id, set in pairs(sets) do
     end
 end
 
+-- Function to count equipped items
 local function countItemsEquipped(ref, items)
     local count = 0
     for _, item in ipairs(items) do
+        mwse.log("Checking if item %s is equipped...", item) -- Debug log
         if mwscript.hasItemEquipped{reference=ref, item=item} then
             count = count + 1
         end
@@ -36,22 +43,27 @@ local function countItemsEquipped(ref, items)
     return count
 end
 
+-- Function to add a spell
 local function addSpell(t)
     for _, spell in ipairs(t.spell) do
+        mwse.log("Trying to add spell: %s", spell) -- Debug log
         if not mwscript.getSpellEffects{reference=t.reference, spell=spell} then
             mwscript.addSpell{reference=t.reference, spell=spell}
         end
     end
 end
 
+-- Function to remove a spell
 local function removeSpell(t)
     for _, spell in ipairs(t.spell) do
+        mwse.log("Trying to remove spell: %s", spell) -- Debug log
         if mwscript.getSpellEffects{reference=t.reference, spell=spell} then
             mwscript.removeSpell{reference=t.reference, spell=spell}
         end
     end
 end
 
+-- Function to add set bonus
 local function addSetBonus(set, ref, numEquipped)
     if numEquipped >= 6 then
         removeSpell{reference=ref, spell=set.minBonus}
@@ -65,6 +77,7 @@ local function addSetBonus(set, ref, numEquipped)
     end
 end
 
+-- Function to handle change in equipped items
 local function equipsChanged(e)
     local id = e and e.item and e.item.id
     if not id then return end
@@ -77,16 +90,19 @@ local function equipsChanged(e)
     addSetBonus(set, e.reference, numEquipped)
 end
 
+-- Registering events
 event.register("equipped", equipsChanged)
 event.register("unequipped", equipsChanged)
 event.register("loaded", equipsChanged)
 
+-- Function to handle NPC load event
 local function npcLoaded(e)
     if not e.reference then return end
     local set_counts = {}
 
     for stack in tes3.iterate(e.reference.object.equipment) do
         if stack.object.objectType == tes3.objectType.armor then
+            mwse.log("Checking equipment stack: %s", stack.object.id) -- Debug log
             local set = setLinks[stack.object.id:lower()]
             if set then
                 set_counts[set] = (set_counts[set] or 0) + 1
@@ -95,9 +111,11 @@ local function npcLoaded(e)
     end
 
     for set, count in pairs(set_counts) do
+        mwse.log("Adding set bonus for set: %s", set.name) -- Debug log
         addSetBonus(set, e.reference, count)
     end
 end
 
+-- Registering events
 event.register("mobileActivated", npcLoaded)
 event.register("loaded", npcLoaded)
