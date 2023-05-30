a1e232039039b415e422e5a1ea06661eeecd7d0b
staticnation <staticnation@gmail.com>
2023-05-30 07:15:31 -0700

    Add files via upload

--- MWSE/mods/Static/SetBonus/config.lua
+++ MWSE/mods/Static/SetBonus/config.lua
@@ -1,9 +1,3 @@
--- config.lua: This module is a configuration data container for the Set Bonus mod.
--- This configuration file contains three tables:
--- `sets`: This table stores registered set data, where each key is a set name and its value is the corresponding set data.
--- `setLinks`: This is a lookup table used to quickly find the set data associated with a specific item ID. In this table, item IDs are keys and the corresponding set data are values.
--- `setsArray`: This table is an empty array initially which can be used for storing or organizing additional data related to the sets.
-
 local config = {
     sets = {},
     setLinks = {},
