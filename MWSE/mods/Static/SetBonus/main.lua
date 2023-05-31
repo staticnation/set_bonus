11de3358291f8f413a83552ca3ee69580294b345
staticnation <staticnation@gmail.com>
2023-05-30 22:52:28 -0700

    Update main.lua

--- MWSE/mods/Static/SetBonus/main.lua
+++ MWSE/mods/Static/SetBonus/main.lua
@@ -2,7 +2,6 @@
 --- The 'config' module holds the configuration data for the Set Bonus mod
 local log = require("Static.logger")
 local config = require("Static.SetBonus.config")
-
 -- 'countItemsEquipped' function checks if a character reference has each item from a given list equipped
 ---@param ref tes3reference
 ---@param items tes3item[]
@@ -13,7 +12,6 @@ local function countItemsEquipped(ref, items)
         return 0
     end
     local count = 0
-
     -- Loop over the items
     for _, item in ipairs(items) do
         -- Check if the item is equipped by the given reference (typically a character)
@@ -26,7 +24,10 @@ local function countItemsEquipped(ref, items)
     log:debug("countItemsEquipped: Counted %s equipped items for the reference: %s", count, ref)
     return count -- Returns the count of equipped items
 end
-
+-- 'addSetBonus' function applies a set bonus based on the number of items a character has equipped from the same set
+---@param set table
+---@param ref tes3reference
+---@param numEquipped number
 local function addSetBonus(set, ref, numEquipped)
     log:info("addSetBonus: Entry point")
     -- Remove all bonuses initially
@@ -68,7 +69,6 @@ local function addSetBonus(set, ref, numEquipped)
     end
     log:info("addSetBonus: Exit point")
 end
-
 -- 'equipsChanged' function handles the event when a character's equipment changes
 ---@param e equippedEventData
 local function equipsChanged(e)
@@ -78,13 +78,11 @@ local function equipsChanged(e)
     if not id then
         return
     end
-
     -- Find the set that the item belongs to
     local linkSet = config.setLinks[id:lower()]
     if not linkSet then
         return
     end
-
     -- Iterate over each set in the linkSet dictionary
     for setName, _ in pairs(linkSet) do
         local set = config.sets[setName]
@@ -102,18 +100,15 @@ local function equipsChanged(e)
     end
     log:info("equipsChanged: Exit point")
 end
-
 -- Registering events to call 'equipsChanged' function when equipment changes
 event.register("equipped", equipsChanged)
 event.register("unequipped", equipsChanged)
-
 -- 'npcLoaded' function handles the event when an NPC is loaded into the game
 ---@param e mobileActivatedEventData
 local function npcLoaded(e)
     log:info("npcLoaded: Entry point")
     if not e.reference then return end
     if not e.reference.object.equipment then return end
-
     -- Create a table to store the count of items from each set the NPC has equipped
     local setCounts = {}
     local allowedTypes = {
@@ -121,7 +116,6 @@ local function npcLoaded(e)
       [tes3.objectType.clothing] = true,
       [tes3.objectType.weapon] = true
     }
-
     for _, stack in pairs(e.reference.object.equipment) do
         if allowedTypes[stack.object.objectType] then
             local keySets = config.setLinks[stack.object.id:lower()] -- Ensure the ID is in lowercase
@@ -132,7 +126,6 @@ local function npcLoaded(e)
             end
         end
     end
-
     -- For each set, apply the set bonus
     for setName, count in pairs(setCounts) do
         local set = config.sets[setName:lower()]
@@ -142,7 +135,6 @@ local function npcLoaded(e)
     end
     log:info("npcLoaded: Exit point")
 end
-
 -- Registering events to call 'npcLoaded' function when an NPC is loaded
 event.register(tes3.event.mobileActivated, npcLoaded)
 event.register(tes3.event.loaded, npcLoaded)
