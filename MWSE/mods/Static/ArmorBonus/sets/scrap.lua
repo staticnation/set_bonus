3605d7e319c2d05c03d087da48d44fc4ac484549
staticnation <staticnation@gmail.com>
2023-05-30 22:44:32 -0700

    Add files via upload

--- MWSE/mods/Static/ArmorBonus/sets/scrap.lua
+++ MWSE/mods/Static/ArmorBonus/sets/scrap.lua
@@ -2,6 +2,6 @@
         name = "Scrap",
         items = { "T_Dwe_Scrap_boots", "T_Dwe_Scrap_Bracer_L", "T_Dwe_Scrap_Bracer_R", "T_Dwe_Scrap_cuirass", "T_Dwe_Scrap_greaves", "T_Dwe_Scrap_helmet", "T_Dwe_Scrap_Pauldron_L", "T_Dwe_Scrap_Pauldron_R", "NOD_RV_dwemscr_boots", "NOD_RV_dwemscr_bracer_left", "NOD_RV_dwemscr_bracer_right", "NOD_RV_dwemscr_cuirass", "NOD_RV_dwemscr_helmet", "NOD_RV_dwemscr_greaves", "NOD_RV_dwemscr_pauldron_left", "NOD_RV_dwemscr_pauldron_right" },
         minBonus = "_sn_scrap_min",
-        maxBonus = "_sn_scrap_mid",
-        midBonus = "_sn_scrap_max"
+        midBonus = "_sn_scrap_mid",
+        maxBonus = "_sn_scrap_max"
 }
