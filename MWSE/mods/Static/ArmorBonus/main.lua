14f31ad27af4c7af51c1df42ff103102d30a8ec9
staticnation <staticnation@gmail.com>
2023-05-31 00:16:40 -0700

    Add files via upload

--- MWSE/mods/Static/ArmorBonus/main.lua
+++ MWSE/mods/Static/ArmorBonus/main.lua
@@ -1,18 +1,4 @@
 -- Import required modules
-local config = require("Static.SetBonus.config")
 local interop = require("Static.SetBonus.interop")
-
 -- 'initAll' function from 'interop' module is used to initialize all set files in a specific directory
 interop.initAll("Data Files/MWSE/mods/Static/ArmorBonus/sets")
-
-
---[[ 
-SUMMARY:
-
-1. The 'config' and 'interop' modules from the 'Static.SetBonus' package are required at the beginning.
-2. The 'initAll' function from the 'interop' module is used to initialize all set files in the specified directory.
-   These files define different sets of items for the Set Bonus mod.
-3. The subsequent loop goes through each set in the 'config.sets' table.
-4. Within each set, it loops over each item and registers the item to its set in the 'setLinks' table.
-5. This provides a way to quickly find which set an item belongs to by looking it up in the 'setLinks' table.
-]]
